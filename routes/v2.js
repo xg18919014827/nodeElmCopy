bf95d8991f1eebc97e357e5207bbc933d91ef5d9
acexu <578599619@qq.com>
2019-12-29 23:00:49 +0800

    增加修改登录密码功能

--- routes/v2.js
+++ routes/v2.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
-import express from 'express';
-import User from '../controller/v2/user';
+import express from "express";
+import User from "../controller/v2/user";
 
 const router = express.Router();
 
-router.post('/login', User.login);
-
+router.post("/login", User.login);
+router.post("/changepassword", User.chanegPassword);
 
 export default router;
